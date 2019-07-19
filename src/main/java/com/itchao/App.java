0ef4d6f18c5e96ac4c31cca6d7a88fb994e70b69
111 <111>
2019-07-19 21:53:55 +0800

    111

--- src/main/java/com/itchao/App.java
+++ src/main/java/com/itchao/App.java
@@ -1,7 +1,13 @@
 package com.itchao;
 
+import java.sql.SQLOutput;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello");
+    }
+
+    public void save(){
+        System.out.println("我是1.0");
     }
 }
